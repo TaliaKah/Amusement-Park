f6ec4668cd3eebfb4108a14219818d7d3bf11ff3
TaliaKah <anaissaratagougne@tutanota.com>
2023-10-25 11:58:24 +0200

    🚨 [Visitor] Fix class name

--- Assets/Visitor.cs
+++ Assets/Visitor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VisitorTest : MonoBehaviour
+public class Visitor : MonoBehaviour
 {
     private UnityEngine.AI.NavMeshAgent agent;
 
