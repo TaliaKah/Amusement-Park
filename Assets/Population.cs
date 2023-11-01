0d80bd9bc1aabd6be726297a01b17dc5b35b4688
TaliaKah <anaissaratagougne@tutanota.com>
2023-11-01 16:06:11 +0100

    ✨ severals visitors

--- Assets/Population.cs
+++ Assets/Population.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Population : MonoBehaviour
 {
+    public int numberOfVisitors = 10;
+    public GameObject visitorPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < numberOfVisitors; i++)
+        {
+            GameObject newVisitor = Instantiate(visitorPrefab, visitorPrefab.transform.position, Quaternion.identity);
+            newVisitor.transform.parent = transform;
+        }
     }
 
     // Update is called once per frame
